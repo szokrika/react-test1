2d52835f57d778d274243ea03a52d4deb0b9b868
Csongor Széles <szokrika@gmail.com>
2020-03-11 16:29:17 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-test1
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    const { show } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        {show ? <span>show</span> : <span>hide</span>}
+        <button onClick={this.toggle}>toggle</button>
+      </div>
+    );
+  }
+}
+
+export default App;
